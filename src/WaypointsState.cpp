f59a24a4624a4f398927ad6bbefb8e79c9f5aca0
thealaaeldin <116704618+Alaaeldinn@users.noreply.github.com>
2023-12-03 23:22:56 +0000

    Update WaypointsState.cpp

--- src/WaypointsState.cpp
+++ src/WaypointsState.cpp
@@ -2,4 +2,8 @@
 
 void UserInteractionHandler::WaypointsState::handleState(UserInteractionHandler* context) {
     // Logic for handling WaypointsState
+    for (const auto& waypoint : context->getWaypoints()) {
+        // Perform sensor fusion logic for each waypoint
+        // Example: context->performSensorFusion(waypoint);
+    }
 }
