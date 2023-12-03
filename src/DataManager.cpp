b4ac193083fa4ad4965842a77c2728768ddb9f5f
thealaaeldin <116704618+Alaaeldinn@users.noreply.github.com>
2023-12-03 23:05:51 +0000

    Update DataManager.cpp

--- src/DataManager.cpp
+++ src/DataManager.cpp
@@ -11,29 +11,30 @@ DataManager& DataManager::getInstance() {
 // Define function to record and save sensor data
 // DataManager.cpp (Source code file)
 
-void DataManager::recordSensorData() {
+pcl::PointCloud<pcl::PointXYZ>::Ptr DataManager::recordSensorData(const std::vector<MyPoint>& customPointCloud)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pclPointCloud(new pcl::PointCloud<pcl::PointXYZ>);
 
+    pclPointCloud->reserve(customPointCloud.size()); // Reserve space for all points at once
+
+    for (const auto& point : customPointCloud)
+    {
+        pclPointCloud->emplace_back(point.x, point.y, point.z);
+    }
+
+    // Further processing or storage as needed.
+
+    return pclPointCloud;
+}
+
+
+// Define function to manage export of maps and pose data
+void DataManager::exportData() {
     // Create a point cloud data structure (e.g., pcl::PointCloud<pcl::PointXYZ>)
     pcl::PointCloud<pcl::PointXYZ> sensorData;
 
     // Save point cloud data to a PCD file
     pcl::io::savePCDFileASCII("sensor_data.pcd", sensorData);
+    
 }
 
-// Define function to manage export of maps and pose data
-void DataManager::exportData(const std::map<std::string, std::string>& mapData, const std::string& poseData) {
-    // Implementation for managing export of maps and pose data
-    std::cout << "Exporting map data and pose data" << std::endl;
-}
-
-// Define function to support standard data formats
-void DataManager::supportStandardFormats() {
-    // Implementation for supporting standard data formats
-    std::cout << "Supporting standard data formats" << std::endl;
-}
-
-// Define function to provide tools for efficient data storage and retrieval
-void DataManager::efficientDataStorage() {
-    // Implementation for efficient data storage and retrieval
-    std::cout << "Providing tools for efficient data storage and retrieval" << std::endl;
-}
