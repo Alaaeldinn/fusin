c1e67916a62580be2e11ebb6787f525201539a8c
thealaaeldin <116704618+Alaaeldinn@users.noreply.github.com>
2023-12-03 23:35:27 +0000

    Update SensorFusion.cpp

--- src/SensorFusion.cpp
+++ src/SensorFusion.cpp
@@ -1,25 +1,23 @@
 #include "SensorFusion.h"
 #include <iostream>
 
-class FusionX : public SensorFusion {
-public:
-    void integrateSensorData(const std::vector<double>& data) override {
-        std::cout << "Default integration of sensor data.";
-    }
+void integrateSensorData(const std::vector<double>& data) override {
+    std::cout << "Default integration of sensor data.";
+}
 
-    void extractFeatures(const std::vector<double>& sensorData) override {
-        std::cout << "Default feature extraction from sensor data.";
-    }
+void extractFeatures(const std::vector<double>& sensorData) override {
+    std::cout << "Default feature extraction from sensor data.";
+}
 
-    void matchFeatures(const std::vector<double>& features) override {
-        std::cout << "Default matching features from different SLAM algorithms.";
-    }
+void matchFeatures(const std::vector<double>& features) override {
+    std::cout << "Default matching features from different SLAM algorithms.";
+}
 
-    void applyFilter(const std::vector<double>& fusedData) override {
-        std::cout << "Default generic filtering mechanism for sensor data fusion.";
-    }
+void applyFilter(const std::vector<double>& fusedData) override {
+    std::cout << "Default generic filtering mechanism for sensor data fusion.";
+}
 
-    void handleOutliers(const std::vector<double>& data) override {
-        std::cout << "Default implementation of robust outlier rejection mechanisms.\n";
-    }
-};
+void handleOutliers(const std::vector<double>& data) override {
+    std::cout << "Default implementation of robust outlier rejection mechanisms.\n";
+}
+
