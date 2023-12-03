3e4ab2d8cc9689afe59ca54fbdbaab467ce16063
thealaaeldin <116704618+Alaaeldinn@users.noreply.github.com>
2023-12-03 23:33:09 +0000

    Update SensorFusion.cpp

--- src/SensorFusion.cpp
+++ src/SensorFusion.cpp
@@ -1,34 +1,14 @@
-#ifndef SENSORFUSION_H
-#define SENSORFUSION_H
-
-#include <vector>
-
-class SensorFusion {
-public:
-    virtual ~SensorFusion() = default;
-    virtual void integrateSensorData(const std::vector<double>& data) = 0;
-    virtual void extractFeatures(const std::vector<double>& sensorData) = 0;
-    virtual void matchFeatures(const std::vector<double>& features) = 0;
-    virtual void applyFilter(const std::vector<double>& fusedData) = 0;
-    virtual void handleOutliers(const std::vector<double>& data) = 0;
-};
-
-#endif // SENSORFUSION_H
-SensorFusion.cpp:
-
-cpp
-Copy code
 #include "SensorFusion.h"
 #include <iostream>
 
 class FusionX : public SensorFusion {
 public:
     void integrateSensorData(const std::vector<double>& data) override {
-        std::cout << "Default integration of sensor data.\n";
+        std::cout << "Default integration of sensor data.";
     }
 
     void extractFeatures(const std::vector<double>& sensorData) override {
-        std::cout << "Default feature extraction from sensor data.\n";
+        std::cout << "Default feature extraction from sensor data.";
     }
 
     void matchFeatures(const std::vector<double>& features) override {
