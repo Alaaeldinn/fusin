cc102839c198833f263080afde3981b3e97dee34
thealaaeldin <116704618+Alaaeldinn@users.noreply.github.com>
2023-12-06 01:19:02 +0200

    Update MapVisualizer.h

--- include/MapVisualizer.h
+++ include/MapVisualizer.h
@@ -1,9 +1,9 @@
 #include "MapSubject.h"
-
+#include <Thirdparty/Open3D/Open3D.h>
 class MapVisualizer : public MapObserver {
 public:
     void update() override;
-    void displayRealTimeMap();
+    void displayRealTimeMap(const open3d::geometry::PointCloud &point_cloud);
     void switchMap(int type);
     void visualizeRobotPose(double pose, double uncertainty);
     void zoomIn();
