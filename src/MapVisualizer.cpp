5dd807ea1b4ff90eaf17db6b39fdbcbc2c8ff980
thealaaeldin <116704618+Alaaeldinn@users.noreply.github.com>
2023-12-06 01:59:53 +0200

    Update MapVisualizer.cpp

--- src/MapVisualizer.cpp
+++ src/MapVisualizer.cpp
@@ -1,81 +1,92 @@
 #include "include/MapVisualizer.h"
 
 void MapVisualizer::update() {
-    displayRealTimeMap();
+    try {
+        displayRealTimeMap();
+    } catch (const std::exception &e) {
+        std::cerr << "Error updating map: " << e.what() << std::endl;
+    }
 }
 
 void MapVisualizer::display3dPoint() {
-    open3d::visualization::Visualizer visualizer;
-    visualizer.CreateVisualizerWindow("Real-Time Point Cloud Map", 1280, 720);
-    // Add the point cloud to the visualizer
-    visualizer.AddGeometry(point_cloud);
-    // Set up the rendering options (optional)
-    open3d::visualization::RenderOption &ro = visualizer.GetRenderOption();
-    ro.point_size_ = 1.0;
+    try {
+        if (!point_cloud) {
+            throw std::runtime_error("Point cloud is not initialized.");
+        }
 
-    // Run the visualization loop
-    // main while loop
-    while (!visualizer.WasStopped()) {
-        visualizer.UpdateGeometry();
-        visualizer.PollEvents();
-        visualizer.UpdateRender();
+        open3d::visualization::Visualizer visualizer;
+        visualizer.CreateVisualizerWindow("Real-Time Point Cloud Map", 1280, 720);
+        visualizer.AddGeometry(point_cloud);
+
+        open3d::visualization::RenderOption &ro = visualizer.GetRenderOption();
+        ro.point_size_ = 1.0;
+
+        while (!visualizer.WasStopped()) {
+            visualizer.UpdateGeometry();
+            visualizer.PollEvents();
+            visualizer.UpdateRender();
+        }
+    } catch (const std::exception &e) {
+        std::cerr << "Error displaying 3D point cloud: " << e.what() << std::endl;
     }
 }
 
 void MapVisualizer::display2dPoint() {
-    // Create a visualization window
-    open3d::visualization::Visualizer visualizer;
-    visualizer.CreateVisualizerWindow("Real-Time Point Cloud Map", 1280, 720);
+    try {
+        if (!point_cloud) {
+            throw std::runtime_error("Point cloud is not initialized.");
+        }
 
-    // Project the 3D point cloud onto the XY plane (set z component to zero)
-    open3d::geometry::PointCloud projected_point_cloud;
-    projected_point_cloud.points_ = point_cloud.points_;
-    for (auto &point : projected_point_cloud.points_) {
-        point[2] = 0.0;
-    }
+        open3d::visualization::Visualizer visualizer;
+        visualizer.CreateVisualizerWindow("Real-Time Point Cloud Map", 1280, 720);
 
-    // Add the 2D point cloud to the visualizer
-    visualizer.AddGeometry(projected_point_cloud);
+        open3d::geometry::PointCloud projected_point_cloud;
+        projected_point_cloud.points_ = point_cloud.points_;
+        for (auto &point : projected_point_cloud.points_) {
+            point[2] = 0.0;
+        }
 
-    // Set up the rendering options (optional)
-    open3d::visualization::RenderOption &ro = visualizer.GetRenderOption();
-    ro.point_size_ = 1.0;
+        visualizer.AddGeometry(projected_point_cloud);
 
-    // Run the visualization loop
-    while (!visualizer.WasStopped()) {
-        visualizer.UpdateGeometry();
-        visualizer.PollEvents();
-        visualizer.UpdateRender();
+        open3d::visualization::RenderOption &ro = visualizer.GetRenderOption();
+        ro.point_size_ = 1.0;
+
+        while (!visualizer.WasStopped()) {
+            visualizer.UpdateGeometry();
+            visualizer.PollEvents();
+            visualizer.UpdateRender();
+        }
+    } catch (const std::exception &e) {
+        std::cerr << "Error displaying 2D point cloud: " << e.what() << std::endl;
     }
 }
 
 void MapVisualizer::switchMap(int type) {
     try {
+        if (type != TWO_D_MAP && type != THREE_D_MAP) {
+            throw std::invalid_argument("Invalid map type. Cannot switch map.");
+        }
+
         if (type == TWO_D_MAP) {
             display2dPoint();
         } else if (type == THREE_D_MAP) {
             display3dPoint();
-        } else {
-            // Handle invalid type
-            throw std::invalid_argument("Invalid map type. Cannot switch map.");
         }
     } catch (const std::exception &e) {
-        std::cerr << "Error: " << e.what() << std::endl;
+        std::cerr << "Error switching map: " << e.what() << std::endl;
     }
 }
 
-
-    
 open3d::geometry::PointCloud visualizeRobotPose() {
-        // Create a point cloud
+    try {
         open3d::geometry::PointCloud pointCloud;
 
-        // Set robot pose (replace these values with your actual robot pose)
         Eigen::Vector3d robotPose(0.0, 0.0, 0.0);
-
-        // Add a single point to represent the robot pose
         pointCloud.points_.push_back(robotPose);
 
-        // Return the created point cloud
         return pointCloud;
+    } catch (const std::exception &e) {
+        std::cerr << "Error visualizing robot pose: " << e.what() << std::endl;
+        return open3d::geometry::PointCloud();  // Return an empty point cloud on error
+    }
 }
