db81a2572faa2bd0c98304935d35d5f3552d4871
thealaaeldin <116704618+Alaaeldinn@users.noreply.github.com>
2023-12-06 01:18:10 +0200

    Update MapVisualizer.cpp

--- src/MapVisualizer.cpp
+++ src/MapVisualizer.cpp
@@ -4,8 +4,22 @@ void MapVisualizer::update() {
     displayRealTimeMap();
 }
 
-void MapVisualizer::displayRealTimeMap() {
-    std::cout << "Displaying real-time map..." << std::endl;
+void MapVisualizer::displayRealTimeMap(const open3d::geometry::PointCloud &point_cloud) {
+    open3d::visualization::Visualizer visualizer;
+    visualizer.CreateVisualizerWindow("Real-Time Point Cloud Map", 1280, 720);
+    // Add the point cloud to the visualizer
+    visualizer.AddGeometry(point_cloud);
+    // Set up the rendering options (optional)
+    open3d::visualization::RenderOption &ro = visualizer.GetRenderOption();
+    ro.point_size_ = 1.0;
+
+    // Run the visualization loop
+    // main while loop
+    while (!visualizer.WasStopped()) {
+        visualizer.UpdateGeometry();
+        visualizer.PollEvents();
+        visualizer.UpdateRender();
+    }
 }
 
 void MapVisualizer::switchMap(int type) {
